7f0a65642f3aec7936310e900700e1b529026e86
Ashley McKemie <a.mckemie@utexas.edu>
2014-05-07 15:27:59 -0500

    Created module method check_if_pl to see if inputted word is already pig latin

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -2,5 +2,11 @@ require "pig_latin/version"
 
 module PigLatin
   # Your code goes here...
-
+  def self.check_if_pl(word)
+    if /\A[aeiou]/.match(word) && /(ay)\z/.match(word)
+      true
+    else
+      false
+    end
+  end
 end
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 require 'pry-debugger'
 
 describe "pig_latin" do
+  describe 'check_if_pl' do
+    it "returns true if inputted word is pig latin" do
+      expect(PigLatin.check_if_pl("eggway")).to eq(true)
+    end
+
+    it "returns false if inputted word is not pig latin" do
+      expect(PigLatin.check_if_pl("yellow")).to eq(false)
+    end
+  end
+
   describe "translate" do
     it "adds 'ay' to the end of a word" do
     end
